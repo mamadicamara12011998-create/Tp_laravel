262efc780966a91eb21992a5807659fe25730f57
Mamadi-camara <mamadicamara12011998@gmail.com>
2026-02-04 13:00:39 +0000

    Premier commit - Projet Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <section>
+             <div>
+       <h1> bonjour je suis etudiant agrotic</h1> 
+    </div>
+        </section>
+   
+    
+    </body>
+</html>
