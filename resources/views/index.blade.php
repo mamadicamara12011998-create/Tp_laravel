0180459b77adf14b1f295a97fcf0a4e9475ed159
Mamadi-camara <mamadicamara12011998@gmail.com>
2026-02-06 10:48:45 +0000

    Creaction_table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index php</title>
+    <title>TP LARAVAL</title>
 </head>
 <body>
-<h1>Bienvenue sur notre page INDEXs</h1>
+<h2>Bienvenue dans la page</h2>
+<img src="/images/images (5).jpg" alt="Deventure de mon site">
 </body>
 </html>
